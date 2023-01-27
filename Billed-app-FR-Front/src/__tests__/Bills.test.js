4f4efd88afa8da8a7fd0e43b4423401ea65e5a9d
Matthieu Mary <matthieu.mary2@gmail.com>
2023-01-27 21:09:54 +0100

    add modal tests on Bills,

--- Billed-app-FR-Front/src/__tests__/Bills.test.js
+++ Billed-app-FR-Front/src/__tests__/Bills.test.js
@@ -5,7 +5,7 @@
 import { screen, waitFor } from "@testing-library/dom";
 import BillsUI from "../views/BillsUI.js";
 import { bills } from "../fixtures/bills.js";
-import { ROUTES_PATH } from "../constants/routes.js";
+import { ROUTES_PATH, ROUTES } from "../constants/routes.js";
 import { localStorageMock } from "../__mocks__/localStorage.js";
 import router from "../app/Router.js";
 import store from "../__mocks__/store.js";
@@ -43,15 +43,38 @@ describe("Given I am connected as an employee", () => {
       expect(dates).toEqual(datesSorted);
     });
     test("Then modal should append and display the right bill document when user click on eye icon", () => {
+      window.localStorage.setItem('user', JSON.stringify({
+        type: 'Employee'
+      }))
       document.body.innerHTML = BillsUI({ data: bills });
+      const onNavigate = (pathname) => {
+        document.body.innerHTML = ROUTES({ pathname })
+      }
+      const currentBills = new Bills ({ document, onNavigate, store: store, localStorage: window.localStorage })
       $.fn.modal = jest.fn();
       const modalSpy = jest.spyOn($.fn, "modal");
       const eyeIcons = screen.getAllByTestId("icon-eye");
-      eyeIcons.map((icon) => userEvent.click(icon));
+      eyeIcons.map( icon => userEvent.click(icon));
       expect(modalSpy).toHaveBeenCalledTimes(4);
     });
     test("Then click on new bill button should display newbill form page", () => {
-
+      // Simule connexion en tant qu'employé
+      window.localStorage.setItem("user", JSON.stringify({ type: "Employee" }))
+      // Simule le contenu du DOM
+      document.body.innerHTML = BillsUI({ data: bills });
+      // Affiche le contenu relatif à la route spécifiée
+      const onNavigate = (pathname) => {
+        document.body.innerHTML = ROUTES({ pathname })
+      }
+      // Simule un nouvel objet Bills 
+      const currentBills = new Bills ({ document, onNavigate, store: store, localStorage: window.localStorage })
+      // Fonction simulée avec implémentation
+      const handleClickNewBillTest = jest.fn((e) => currentBills.handleClickNewBill(e))
+      const btnNewBill = screen.getByTestId("btn-new-bill");
+      btnNewBill.addEventListener("click", handleClickNewBillTest)
+      userEvent.click(btnNewBill);
+      const newBillForm = screen.getByTestId("form-new-bill");
+      expect(newBillForm).toBeTruthy();
     })
   });
 });
--- Billed-app-FR-Front/src/views/BillsUI.js
+++ Billed-app-FR-Front/src/views/BillsUI.js
@@ -6,7 +6,7 @@ import Actions from './Actions.js'
 
 const row = (bill) => {
   return (`
-    <tr>
+    <tr data-testid="single-bill">
       <td>${bill.type}</td>
       <td>${bill.name}</td>
       <td data-testid=${bill.date}>${bill.formatedDate}</td>
